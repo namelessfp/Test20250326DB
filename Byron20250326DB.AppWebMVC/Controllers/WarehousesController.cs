e18b82cc314cb1c718bb70fc3c94c22de3a24011
ALUMNOS <ALUMNOS@M04-CII>
2025-03-26 11:47:38 -0600

    bodega creada

--- Byron20250326DB.AppWebMVC/Controllers/WarehousesController.cs
+++ Byron20250326DB.AppWebMVC/Controllers/WarehousesController.cs
@@ -19,10 +19,20 @@ namespace Byron20250326DB.AppWebMVC.Controllers
         }
 
         // GET: Warehouses
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(Warehouse bodega, int topRegistro = 10)
         {
-            return View(await _context.Warehouses.ToListAsync());
+            var query = _context.Warehouses.AsQueryable();
+            if (!string.IsNullOrWhiteSpace(bodega.WarehouseName))
+                query = query.Where(s => s.WarehouseName.Contains(bodega.WarehouseName));
+
+
+            if (topRegistro > 0)
+                query = query.Take(topRegistro);
+
+
+            return View(await query.ToListAsync());
         }
+
 
         // GET: Warehouses/Details/5
         public async Task<IActionResult> Details(int? id)
--- Byron20250326DB.AppWebMVC/Models/Warehouse.cs
+++ Byron20250326DB.AppWebMVC/Models/Warehouse.cs
@@ -1,12 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Byron20250326DB.AppWebMVC.Models;
 
 public partial class Warehouse
 {
     public int Id { get; set; }
-
+    [Display(Name = "Bodega nombre")]
     public string WarehouseName { get; set; } = null!;
 
     public virtual ICollection<Product> Products { get; set; } = new List<Product>();
